4a4ca3cd0dd15a3bc6bf8ee34c737d2f306e9785
101-14 <ekfrlsms22@gmail.com>
2020-04-11 16:23:39 +0900

    pygame 이미지 변수

--- pygame_05.py
+++ pygame_05.py
@@ -6,13 +6,26 @@ width, height = 640, 480
 size = (width, height)
 screen = pygame.display.set_mode(size)
 
-player = pygame.image.load('resources/images/dogdog.png')
-player2 = pygame.image.load('resources/images/insta.png')
+player = pygame.image.load('resources/images/dude.png')
+grass = pygame.image.load('resources/images/mic.png')
+castle = pygame.image.load('resources/images/castle.png')
 
 while True:
     screen.fill((0, 0, 0))
-    screen.blit(player, (10, 10))
-    screen.blit(player2, (330, 10))
+
+    grass_width = grass.get_width()
+    grass_height = grass.get_height()
+    for y in range(height // grass_height + 1):
+        for x in range(width // grass_width + 1):
+            screen.blit(grass, (x * grass_width, y * grass_height))
+
+    castle_height = castle.get_height()
+    for y in range(height // castle_height):
+        screen.blit(castle, (0, 30 + y * castle_height))
+
+
+    screen.blit(player, (100, 100))
+
     pygame.display.flip()
 
     for event in pygame.event.get():
