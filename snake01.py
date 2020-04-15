c85b99d2d4a3a0de39aed44e587d33dbcbdfdbf0
101-14 <happy.myhome1@gmail.com>
2020-04-15 14:37:24 +0900

    pygame 파이게이 창 열기

---
+++ snake01.py
@@ -0,0 +1,12 @@
+import pygame
+import time
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 80
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+pygame.init()
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
+
+time.sleep(3)
