55b8f0e677e1c1728dcf3c40ec4f529d7336dde7
101-14 <happy.myhome1@gmail.com>
2020-04-15 15:10:45 +0900

    pygame 파게임 화면 분할(20px) 함수

---
+++ snake04.py
@@ -0,0 +1,42 @@
+import pygame
+import time
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+BLOCK_SIZE = 20
+
+def draw_block(screen, color, position):
+    block = pygame.Rect((position[0] * BLOCK_SIZE, position[1] * BLOCK_SIZE),
+                        (BLOCK_SIZE, BLOCK_SIZE))
+    pygame.draw.rect(screen, color, block)
+
+pygame.init()
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
+screen.fill(WHITE)
+
+draw_block(screen, RED, (1, 1))
+draw_block(screen, RED, (1, 3))
+draw_block(screen, RED, (1, 5))
+draw_block(screen, RED, (1, 7))
+draw_block(screen, GREEN, (3, 1))
+draw_block(screen, GREEN, (3, 3))
+draw_block(screen, GREEN, (3, 5))
+draw_block(screen, GREEN, (3, 7))
+draw_block(screen, BLUE, (5, 1))
+draw_block(screen, BLUE, (5, 3))
+draw_block(screen, BLUE, (5, 5))
+draw_block(screen, BLUE, (5, 7))
+
+pygame.display.flip()
+
+time.sleep(3)
+
